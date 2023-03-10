66ac425afea53f4e44c0796420949116b94a23dc
Kemal Aslan <kemal.aslan.note@gmail.com>
2023-03-10 20:02:16 +0300

    Initial Commit

---
+++ MVCSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCSampleApp
+//
+//  Created by Kemal Aslan on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
